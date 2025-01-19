8e08ddda095da23afee883370ee53244b18176d0
nandah4 <nyustira6@gmail.com>
2025-01-19 11:41:30 +0700

    feat: add err message when input text less than 1

--- app/src/main/java/com/example/bmicalculator/fragment/FormFragment.kt
+++ app/src/main/java/com/example/bmicalculator/fragment/FormFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.bmicalculator.databinding.FragmentFormBinding
 
@@ -21,6 +22,16 @@ class FormFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
 
+
+        binding.btnSubmitFormName.setOnClickListener {
+            val nameValue = binding.edtFormName.editText?.text.toString().trim()
+            if (nameValue.length < 1) {
+                binding.edtFormName.error = "Character less than 1"
+                return@setOnClickListener
+            } else {
+                Toast.makeText(this.requireActivity(), nameValue, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
